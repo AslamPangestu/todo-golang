90dfe5ca03a916a62faf17b5e8fba5eff1489a02
aslampangestu <aslampangestu@gmail.com>
2024-11-19 21:51:26 +0700

    feat: add put method

--- lib/cors.go
+++ lib/cors.go
@@ -11,7 +11,7 @@ func InitializeCORS() gin.HandlerFunc {
 	var FRONTEND_URL = os.Getenv("FRONTEND_URL")
 	return cors.New(cors.Config{
 		AllowOrigins: []string{FRONTEND_URL},
-		AllowMethods: []string{"GET", "PATCH", "POST", "DELETE"},
+		AllowMethods: []string{"GET", "PATCH", "POST", "PUT", "DELETE"},
 		AllowHeaders: []string{"Authorization", "Content-Type"},
 	})
 }
